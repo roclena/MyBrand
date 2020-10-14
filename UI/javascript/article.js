3e34d1497e450709702763a82b82ac94ae2d9904
roclena <cleronga@yahoo.fr>
2020-10-14 19:26:07 +0200

    ft(save article or post article):artcle.hml

---
+++ UI/javascript/article.js
@@ -0,0 +1,36 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional  
+var firebaseConfig = {
+    apiKey: "AIzaSyCttetuD8PgZQXFCiDQ7w0qcI4PfqknbVM",
+    authDomain: "myweb-64a8a.firebaseapp.com",
+    databaseURL: "https://myweb-64a8a.firebaseio.com",
+    projectId: "myweb-64a8a",
+    storageBucket: "myweb-64a8a.appspot.com",
+    messagingSenderId: "605134341919",
+    appId: "1:605134341919:web:df86688d8634978bbf122a",
+    measurementId: "G-XDE7ZTEJLS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var messagesref = firebase.database().ref('articles');
+document.getElementById('articleform').addEventListener('submit', submitform);
+
+function submitform(e) {
+    e.preventDefault();
+    var title = document.getElementById('title').value;
+    var subject = document.getElementById('subject').value;
+    var PostedDate =Date();
+    savemessage(title, subject, PostedDate);
+    document.getElementById('title').value = '';
+    document.getElementById('subject').value = '';
+
+}
+function savemessage(title, subject, PostedDate) {
+    var newmessref = messagesref.push();
+    newmessref.set({
+        Title: title,
+        Body: subject,
+        PostedDate: PostedDate
+
+    });
+}
