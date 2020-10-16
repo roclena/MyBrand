029c87748b39268ec450cd91a00c22e8cd3e9404
roclena <cleronga@yahoo.fr>
2020-10-16 09:57:19 +0200

    Fix(admin dashboard)

---
+++ UI/javascript/Adminprofile.js
@@ -0,0 +1,24 @@
+var users = JSON.parse(localStorage.getItem('admin'));
+if (users == null) {
+  window.location.href = "AdminLogin.html";
+} else {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCttetuD8PgZQXFCiDQ7w0qcI4PfqknbVM",
+    authDomain: "myweb-64a8a.firebaseapp.com",
+    databaseURL: "https://myweb-64a8a.firebaseio.com",
+    projectId: "myweb-64a8a",
+    storageBucket: "myweb-64a8a.appspot.com",
+    messagingSenderId: "605134341919",
+    appId: "1:605134341919:web:df86688d8634978bbf122a",
+    measurementId: "G-XDE7ZTEJLS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  database = firebase.database();
+
+  document.getElementById('usname').innerHTML = 'Admin :' + (users.firstName).toUpperCase();
+  function logout() {
+    localStorage.clear();
+    window.location.href = "login.html";
+}
+}
